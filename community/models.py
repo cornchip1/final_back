d89d137794f9d95ab4febae1ad628e8b302e320c
배이경 <yikyung52@naver.com>
2023-05-18 10:56:11 +0900

    block another user update/delete posts and comments, add user info in comments

--- community/models.py
+++ community/models.py
@@ -12,6 +12,7 @@ class Article(models.Model):
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
