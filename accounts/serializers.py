c01f7cbfaa63b7e16e9fcf2ec7f2282ba69e3638
배이경 <yikyung52@naver.com>
2023-05-19 10:17:45 +0900

    profile

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
-# from .models import Profile
+from .models import User
+from community.serializers import ArticleSerializer,CommentSerializer
 
 from rest_framework import serializers
 
 from community.serializers import CommentSerializer, ArticleSerializer, ArticleListSerializer
+from community.models import Article, Comment
 
-
-# class ProfileSerializer(serializers.ModelSerializer):
-#     # article_set = ArticleSerializer(many=True, read_only=True)
-#     # article_count = serializers.IntegerField(source = 'article_set.count',read_only=True)
-
-#     class Meta:
-#         model = Profile
-#         fields = '__all__'
+class ProfileSerializer(serializers.ModelSerializer):
+    articles = ArticleSerializer(many=True, read_only=True)
+    articles_count = serializers.IntegerField(source = 'articles.count', read_only = True)
+    comments = CommentSerializer(many=True, read_only=True)
+    comments_count = serializers.IntegerField(source = 'comments.count', read_only = True)
+    
+    class Meta:
+        model = User
+        fields = ('username','date_joined','img','articles','articles_count','comments','comments_count')
+        read_only_fields = ('username',)
+    
