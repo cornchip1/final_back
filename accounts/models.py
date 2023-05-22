b5a0bf211286a193b48eddb4c342e40e04ab2a03
배이경 <yikyung52@naver.com>
2023-05-22 16:58:20 +0900

    default profile and bagkround img

--- accounts/models.py
+++ accounts/models.py
@@ -5,11 +5,12 @@ from datetime import datetime
 
 # Create your models here.
 class User(AbstractUser):
-    img = models.ImageField(upload_to='%Y/%m/%d',null=True)
+    img = models.ImageField(upload_to='profile_img/%Y/%m/%d',null=True, default = 'default_profile.jpg')
+    background_img = models.ImageField(upload_to='background_img/%Y/%m/%d',null=True, default = 'default_background.png')
     
-
 # class Profile(models.Model):
 #     user = models.OneToOneField(User,on_delete=models.CASCADE)
 #     img = models.ImageField(upload_to='%Y/%m/%d')
 #     # article = models.ManyToManyField(Article)
 #     # comment = models.ManyToManyField(Comment)
+ 
